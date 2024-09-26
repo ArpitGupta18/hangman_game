aa5b7cb27b28d22dfdd968c58a4639a2340f6a71
Arpit Gupta <arpitgupta@Arpits-MacBook-Pro.local>
2024-09-26 16:56:02 +0545

    First commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const animals = [
+	"Elephant",
+	"Tiger",
+	"Kangaroo",
+	"Penguin",
+	"Crocodile",
+	"Dolphin",
+	"Giraffe",
+	"Hippopotamus",
+	"Chimpanzee",
+	"Octopus",
+];
+
+const movies = [
+	"Titanic",
+	"Inception",
+	"Gladiator",
+	"The Godfather",
+	"Avatar",
+	"Jurassic Park",
+	"The Matrix",
+	"The Lion King",
+	"Interstellar",
+	"Forrest Gump",
+];
+
+const countries = [
+	"Australia",
+	"Brazil",
+	"Canada",
+	"Denmark",
+	"Egypt",
+	"Finland",
+	"Germany",
+	"India",
+	"Japan",
+	"Switzerland",
+];
+
+// const gameChoice = document.getElementById("gameChoice");
+// const submitBtn = document.getElementById("submitBtn");
+// const choice = "";
+
+// submitBtn.addEventListener("click", (e) => {
+// 	e.preventDefault();
+// 	const choice = gameChoice.value;
+// 	console.log(choice);
+// 	gameChoice.value = "";
+// });
